1eb248d596aeb13a331c3bf5c1a0ae1d3f1761e1
Baidak.D <denis.baidak@gmail.com>
2026-07-09 21:55:40 +0300

    familiar: pin formula to v0.3.0 release tarball

--- Formula/familiar.rb
+++ Formula/familiar.rb
@@ -2,7 +2,7 @@ class Familiar < Formula
   desc "Keyboard-driven kitty overlays for a Claude Code workflow"
   homepage "https://github.com/DenoBY/familiar"
   url "https://github.com/DenoBY/familiar/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "38e81d789f0808b80df203df2382935c15ff84987111e9431e9acfb578924850"
   license "MIT"
   head "https://github.com/DenoBY/familiar.git", branch: "master"
 
